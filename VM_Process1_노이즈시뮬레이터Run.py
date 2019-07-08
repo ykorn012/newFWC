e49144666a880c046257f0e16d805e5712937d41
크게접은하늘 <ykorn012@gmail.com>
2019-07-08 19:26:29 +0900

    Update VM_Process1_노이즈시뮬레이터Run.py

--- VM_Process1_노이즈시뮬레이터Run.py
+++ VM_Process1_노이즈시뮬레이터Run.py
@@ -21,25 +21,25 @@ os.chdir("D:/10. 대학원/04. Source/OnlyVM/03. Local VM/")
 A_p1 = np.array([[0.5, -0.2], [0.25, 0.15]])    #recipe gain matrix
 d_p1 = np.array([[0.1, 0], [0.05, 0]])  #drift matrix
 C_p1 = np.transpose(np.array([[0, 0.5, 0.05, 0, 0.15, 0], [0.085, 0, 0.025, 0.2, 0, 0]])) # FDC variable matrix
-SEED = 100
+SEED = 111999999
 
 M = 10
 Z_DoE = 12
 Z_VM = 40
 Nz_RUN = 15
 
+v_PLS = 0.4
+
 def main():
     fdh_graph = FDC_Graph()
     fwc_p1_vm = VM_Process1_노이즈시뮬레이터(A_p1, d_p1, C_p1, SEED)
-    fwc_p1_vm.DoE_Run(lamda_PLS=0.5, Z=Z_DoE, M=M)  #DoE Run
-    VM_Output, ACT_Output, ez_run, y_act, y_prd = fwc_p1_vm.VM_Run(lamda_PLS=0.5, Z=Z_VM, M=M)
+    fwc_p1_vm.DoE_Run(lamda_PLS=v_PLS, Z=Z_DoE, M=M)  #DoE Run
+    VM_Output, ACT_Output, ez_run, y_act, y_prd = fwc_p1_vm.VM_Run(lamda_PLS=v_PLS, Z=Z_VM, M=M)
 
     fdh_graph.plt_show1(Z_VM * M, y_act[:, 0:1], y_prd[:, 0:1])
     fdh_graph.plt_show2(Z_VM, ez_run[:, 0:1], ez_run[:, 1:2])
 
-    p1_mape_Queue = []
-
-    np.savetxt("output/p1_ez_run.csv", ez_run, delimiter=",", fmt="%.4f")
+    #np.savetxt("output/p1_ez_run.csv", ez_run, delimiter=",", fmt="%.4f")
     # for z in np.arange(22, 34, 1):
     #     mape = fdh_graph.mean_absolute_percentage_error(z + 1, y_act[((z + 1) * M) - 1][0], y_prd[((z + 1) * M) - 1][0])
     #     p1_mape_Queue.append(mape)
@@ -51,11 +51,22 @@ def main():
     #         mape_sum += mape
     #     p1_mape_Queue.append(mape_sum / M)
 
-    for i in np.arange(15 * M, Z_VM * M, 1):
-        mape = fdh_graph.mean_absolute_percentage_error(i + 1, y_act[i][0], y_prd[i][0])
-        p1_mape_Queue.append(mape)
+    p1_q1_mape_Queue = []
 
-    print('MAPE (%) : ', np.mean(p1_mape_Queue))
+    # metrology 마다 보여주는 MAPE 값이 의미가 없다.
+    for z in np.arange(Nz_RUN, Z_VM, 1):
+        mape = fdh_graph.mean_absolute_percentage_error(z + 1, y_act[((z + 1) * M) - 1][0], y_prd[((z + 1) * M) - 1][0])
+        p1_q1_mape_Queue.append(mape)
+
+    print('Process-1 q1 Every Metrology MAPE After 15 Lot : {0:.2f}%'.format(np.mean(p1_q1_mape_Queue)))
+    p1_q1_mape_Queue = []
+
+    for i in np.arange(Nz_RUN * M, Z_VM * M, 1):
+        mape = fdh_graph.mean_absolute_percentage_error(i + 1, y_act[i][0], y_prd[i][0])
+        p1_q1_mape_Queue.append(mape)
+
+    print('Process-1 q1 All MAPE After 15 Lot : {0:.2f}%'.format(np.mean(p1_q1_mape_Queue)))
+
     # np.savetxt("output/noise_mape_Queue.csv", p1_mape_Queue, delimiter=",", fmt="%.4f")
     # np.savetxt("output/abNormal_VM_Output.csv", VM_Output, delimiter=",", fmt="%.4f")
 
