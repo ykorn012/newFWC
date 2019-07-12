59adebacc2963b73e649d11de365dcfdfdc3647f
크게접은하늘 <ykorn012@gmail.com>
2019-07-12 18:25:51 +0900

    Update VM_Process1_DynamicSampling_Run.py

--- VM_Process1_DynamicSampling_Run.py
+++ VM_Process1_DynamicSampling_Run.py
@@ -12,10 +12,7 @@
 import numpy as np
 from simulator.VM_Process1_DynamicSampling_시뮬레이터 import VM_Process1_DynamicSampling_시뮬레이터
 from simulator.FDC_Graph import FDC_Graph
-# from pandas import DataFrame, Series
-# import pandas as pd
 import os
-from matplotlib import pyplot as plt
 
 os.chdir("D:/10. 대학원/04. Source/OnlyVM/10. DynamicSampling/")
 
@@ -25,20 +22,23 @@ C_p1 = np.transpose(np.array([[0, 0.5, 0.05, 0, 0.15, 0], [0.085, 0, 0.025, 0.2,
 SEED = 1  #3
 
 M = 10
-Z_DoE = 12
-Z_VM = 55
-Nz_RUN = 15
-v_PLS = 1
+S1 = 25
 dM = 5
+S2 = 30
+RUNS_CNT = S1 + S2
+N = M * S1 + dM * S2
 dStart = 26  #25 * 10 + 30 * 5
+Z_DoE = 12
+Nz_RUN = 15
+v_PLS = 0.8
 
 # Process 변수와 출력 관련 system gain matrix
 
 def main():
     fdh_graph = FDC_Graph()
     fwc_p1_vm = VM_Process1_DynamicSampling_시뮬레이터(A_p1, d_p1, C_p1, dM, dStart, SEED)
-    fwc_p1_vm.DoE_Run(lamda_PLS=v_PLS, Z=Z_DoE, M=M)  #DoE Run
-    VM_Output, ACT_Output, ez_run, y_act, y_prd = fwc_p1_vm.VM_Run(lamda_PLS=v_PLS, Z=Z_VM, M=M)
+    fwc_p1_vm.DoE_Run(lamda_PLS=v_PLS, Z=Z_DoE, M=M)  # DoE Run
+    VM_Output, ACT_Output, ez_run, y_act, y_prd = fwc_p1_vm.VM_Run(lamda_PLS=v_PLS, Z=RUNS_CNT, M=M)
 
     np.savetxt("output/VM_Output.csv", VM_Output, delimiter=",", fmt="%.4f")
     np.savetxt("output/ACT_Output.csv", ACT_Output, delimiter=",", fmt="%.4f")
@@ -46,16 +46,27 @@ def main():
     np.savetxt("output/y_act.csv", y_act, delimiter=",", fmt="%.4f")
     np.savetxt("output/y_prd.csv", y_prd, delimiter=",", fmt="%.4f")
 
-    fdh_graph.plt_show1(40 * M, y_act[:, 0:1], y_prd[:, 0:1])
-    fdh_graph.plt_show2(55, ez_run[:, 0:1], ez_run[:, 1:2])
+    fdh_graph.plt_show1(N, y_act[:, 0:1], y_prd[:, 0:1])
+    fdh_graph.plt_show2(RUNS_CNT, ez_run[:, 0:1], ez_run[:, 1:2])
 
     ez_run_out = []
+    runM = M
 
-    for z in range(Z_VM):
-        if z == dM:
-            M = dM
-        for k in np.arange(z * M, (z + 1) * M):
-            ez_run_out[k] = ez_run[z]
+    for z in np.arange(1, RUNS_CNT + 1):
+        if z == dStart:
+            runM = dM
+        for k in np.arange(z * runM, (z + 1) * runM):
+            ez_run_out.append(ez_run[z])
+    ez_run_out = np.array(ez_run_out)
+    np.savetxt("output/ez_run2.csv", ez_run_out, delimiter=",", fmt="%.4f")
+
+    # plt.figure()
+    # plt.plot(np.arange(N), ez_run_out[:, 0:1], 'bx-', ez_run_out[:, 1:2], 'gx--', lw=2, ms=5, mew=2)
+    # plt.xticks(np.arange(0, N, 50))
+    # plt.yticks(np.arange(-1.2, 1.3, 0.2))
+    # plt.xlabel('Metrology Run No.(z)')
+    # plt.ylabel('e(z)')
+
 
 if __name__ == "__main__":
     main()
