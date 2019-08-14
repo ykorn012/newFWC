af89364738f6676198de288819494a93493cb288
크게접은하늘 <ykorn012@gmail.com>
2019-08-14 19:19:54 +0900

    Update VM_Process1_노이즈시뮬레이터Run.py

--- VM_Process1_노이즈시뮬레이터Run.py
+++ VM_Process1_노이즈시뮬레이터Run.py
@@ -16,19 +16,19 @@ from simulator.FDC_Graph import FDC_Graph
 # import pandas as pd
 import os
 
-os.chdir("D:/10. 대학원/04. Source/OnlyVM/03. Local VM/")
+os.chdir("D:/10. 대학원/04. Source/09. VM_Source/03. VMOnly/")
 
 A_p1 = np.array([[0.5, -0.2], [0.25, 0.15]])    #recipe gain matrix
 d_p1 = np.array([[0.1, 0], [0.05, 0]])  #drift matrix
 C_p1 = np.transpose(np.array([[0, 0.5, 0.05, 0, 0.15, 0], [0.085, 0, 0.025, 0.2, 0, 0]])) # FDC variable matrix
-SEED = 111999999
+SEED = 9
 
 M = 10
 Z_DoE = 12
 Z_VM = 40
 Nz_RUN = 15
 
-v_PLS = 0.4
+v_PLS = 0.6
 
 def main():
     fdh_graph = FDC_Graph()
@@ -37,7 +37,7 @@ def main():
     VM_Output, ACT_Output, ez_run, y_act, y_prd = fwc_p1_vm.VM_Run(lamda_PLS=v_PLS, Z=Z_VM, M=M)
 
     fdh_graph.plt_show1(Z_VM * M, y_act[:, 0:1], y_prd[:, 0:1])
-    fdh_graph.plt_show2(Z_VM, ez_run[:, 0:1], ez_run[:, 1:2], Noise=True)
+    fdh_graph.plt_show12(Z_VM, ez_run[:, 0:1], ez_run[:, 1:2])
 
     #np.savetxt("output/p1_ez_run.csv", ez_run, delimiter=",", fmt="%.4f")
     # for z in np.arange(22, 34, 1):
