da1b440055485e4794e60094fdc503b02ee96634
크게접은하늘 <ykorn012@gmail.com>
2019-08-14 19:21:17 +0900

    Update VM_Process1to2_노이즈_업스트림_제어.py

--- VM_Process1to2_노이즈_업스트림_제어.py
+++ VM_Process1to2_노이즈_업스트림_제어.py
@@ -11,14 +11,14 @@
 #======================================================================================================
 import numpy as np
 from simulator.VM_Process1_시뮬레이터 import VM_Process1_시뮬레이터
-from simulator.VM_Process1_DynamicSampling_노이즈시뮬레이터 import VM_Process1_DynamicSampling_노이즈시뮬레이터
+from simulator.VM_Process1_노이즈시뮬레이터 import VM_Process1_노이즈시뮬레이터
 from simulator.VM_Process2_시뮬레이터 import VM_Process2_시뮬레이터
 from simulator.FDC_Graph import FDC_Graph
 import os
 # from pandas import DataFrame, Series
 # import pandas as pd
 
-os.chdir("D:/10. 대학원/04. Source/OnlyVM/10. DynamicSampling/")
+os.chdir("D:/10. 대학원/04. Source/09. VM_Source/03. VMOnly/")
 
 A_p1 = np.array([[0.5, -0.2], [0.25, 0.15]])    #recipe gain matrix
 d_p1 = np.array([[0.1, 0], [0.05, 0]])  #drift matrix
@@ -29,8 +29,8 @@ A_p2 = np.array([[1, 0.1], [-0.5, 0.2]])
 d_p2 = np.array([[0, 0.05], [0, 0.05]])
 C_p2 = np.transpose(np.array([[0.1, 0, 0, -0.2, 0.1], [0, -0.2, 0, 0.3, 0]]))
 F_p2 = np.array([[2, 0], [0, 2]])
-SEED1 = 111999999  #111999999
-SEED2 = 999111111
+#SEED = 10   # 999999999 999999000   100 111999999\
+SEED = 999999999
 # Process 변수와 출력 관련 system gain matrix
 
 M = 10
@@ -38,48 +38,27 @@ Z_DoE = 12
 Z_VM = 40
 Nz_RUN = 15
 
-Upstream_Rule_MAPE = 10
+Upstream_Rule_MAPE = 20
 
-v1_PLS = 0.4
+v1_PLS = 0.6
 v2_PLS = 0.6
 
-M = 10
-S1 = 20
-dM = 5
-S2 = 40
-RUNS_CNT = S1 + S2
-N = M * S1 + dM * S2
-dStart = S1 + 1  #25 * 10 + 30 * 5
-Z_DoE = 12
-v_PLS = 0.4
-Nz_RUN = 15
 
 def main():
     fdh_graph = FDC_Graph()
-    fwc_p1_vm = VM_Process1_시뮬레이터(A_p1, d_p1, C_p1, SEED1)
+    fwc_p1_vm = VM_Process1_시뮬레이터(A_p1, d_p1, C_p1, SEED)
     fwc_p1_vm.DoE_Run(lamda_PLS=v1_PLS, Z=Z_DoE, M=M)  # DoE Run
     Normal_VMResult, Normal_ACTResult, ez_run, o_y_act, o_y_prd = fwc_p1_vm.VM_Run(lamda_PLS=v1_PLS, Z=Z_VM, M=M)
 
-    fwc_p1_vm = VM_Process1_DynamicSampling_노이즈시뮬레이터(A_p1, d_p1, C_p1, dM, dStart, SEED1)
-    fwc_p1_vm.DoE_Run(lamda_PLS=v_PLS, Z=Z_DoE, M=M)  # DoE Run
-    VM_Output, ACT_Output, ez_run, p1_y_act, p1_y_prd = fwc_p1_vm.VM_Run(lamda_PLS=v_PLS, Z=RUNS_CNT, M=M)
+    fwc_p1_vm = VM_Process1_노이즈시뮬레이터(A_p1, d_p1, C_p1, SEED)
+    fwc_p1_vm.DoE_Run(lamda_PLS=v1_PLS, Z=Z_DoE, M=M)  # DoE Run
+    Error_VMresult, Error_ACTResult, ez_run, p1_y_act, p1_y_prd = fwc_p1_vm.VM_Run(lamda_PLS=v1_PLS, Z=Z_VM, M=M)
 
-    fdh_graph.plt_show1(Z_VM * M, p1_y_act[:, 0:1], p1_y_prd[:, 0:1])
-    ez_run_out = []
-    runM = M
+    # fdh_graph.plt_show1(Z_VM * M, p1_y_act[:, 0:1], p1_y_prd[:, 0:1])
+    # fdh_graph.plt_show2(Z_VM, ez_run[:, 0:1], ez_run[:, 1:2])
 
-    ez_run_out.append(np.array([0, 0]))
-    for z in np.arange(1, RUNS_CNT + 1):
-        if z == dStart:
-            runM = dM
-        for k in np.arange(z * runM, (z + 1) * runM):
-            ez_run_out.append(ez_run[z])
-    ez_run_out = np.array(ez_run_out)
-
-    fdh_graph.plt_show5(ez_run_out, N, M, dM, S1, Noise=True)
-
-    np.savetxt("output/Error_VMresult2.csv", VM_Output, delimiter=",", fmt="%.4f")
-    np.savetxt("output/Error_ACTResult2.csv", ACT_Output, delimiter=",", fmt="%.4f")
+    np.savetxt("output/Error_VMresult2.csv", Error_VMresult, delimiter=",", fmt="%.4f")
+    np.savetxt("output/Error_ACTResult2.csv", Error_ACTResult, delimiter=",", fmt="%.4f")
 
     # for z in np.arange(21, 32, 1):
     #     for k in np.arange(z * M, (z + 1) * M):
@@ -111,25 +90,21 @@ def main():
         p1_q1_mape_Queue.append(mape)
 
     np.savetxt("output/p1_mape.csv", p1_q1_mape_Queue, delimiter=",", fmt="%.4f")
-    np.savetxt("output/Error_VMresult1.csv", VM_Output, delimiter=",", fmt="%.4f")
+    np.savetxt("output/Error_VMresult1.csv", Error_VMresult, delimiter=",", fmt="%.4f")
     np.savetxt("output/p1_y_prd.csv", p1_y_prd, delimiter=",", fmt="%.4f")
     np.savetxt("output/p1_y_act.csv", p1_y_act, delimiter=",", fmt="%.4f")
-    np.savetxt("output/Error_ACTResult.csv", ACT_Output, delimiter=",", fmt="%.4f")
+    np.savetxt("output/Error_ACTResult.csv", Error_ACTResult, delimiter=",", fmt="%.4f")
 
     for z in np.arange(Nz_RUN, Z_VM, 1):
         if p1_q1_mape_Queue[z] >= Upstream_Rule_MAPE:
             x1 = p1_y_act[(z * M) - 1]
             x2 = p1_y_act[((z + 1) * M) - 1]
             s = (x2 - x1) / M
-            act_q1 = np.abs(x2[0] - x1[0])
-            act_q2 = np.abs(x2[1] - x1[1])
-            if act_q1 <= 4 or act_q2 <= 4:
-                for k in np.arange(z * M, (z + 1) * M - 1, 1):
-                    i = k % (z * M) + 1
-                    #p1_y_prd[k] = x1 + s * i
-                    p1_y_prd[k] = x2
-                    #print('test : ', x1, x2, p1_y_prd[k])
-                    #Error_VMresult[k] = Error_VMresult[((z + 1) * M) - 1]
+            for k in np.arange(z * M, (z + 1) * M - 1, 1):
+                i = k % (z * M) + 1
+                p1_y_prd[k] = x1 + s * i
+                #print('test : ', x1, x2, p1_y_prd[k])
+                #Error_VMresult[k] = Error_VMresult[((z + 1) * M) - 1]
 
     # for z in np.arange(15, 40, 1):
     #     if p1_mape_Queue[z] >= 10:
@@ -138,12 +113,12 @@ def main():
 
     np.savetxt("output/Error_VMresult_OK.csv", p1_y_prd, delimiter=",", fmt="%.4f")
 
-    fwc_p2_act = VM_Process2_시뮬레이터(A_p2, d_p2, C_p2, F_p2, v1_PLS, p1_y_prd, p1_y_act, SEED2)
+    fwc_p2_act = VM_Process2_시뮬레이터(A_p2, d_p2, C_p2, F_p2, v1_PLS, p1_y_prd, p1_y_act, SEED)
     fwc_p2_act.DoE_Run(lamda_PLS=v2_PLS, Z=Z_DoE, M=M, f=o_y_act)  # DoE Run ACT값으로 가능
     p2_VM_Output, p2_ACT_Output, p2_ez_run, p2_y_act, p2_y_prd = fwc_p2_act.VM_Run(lamda_PLS=v2_PLS, Z=Z_VM, M=M)
 
     fdh_graph.plt_show1(Z_VM * M, p2_y_act[:, 1:2], p2_y_prd[:, 1:2])
-    fdh_graph.plt_show2(Z_VM, p2_ez_run[:, 0:1], p2_ez_run[:, 1:2], Noise=True)
+    fdh_graph.plt_show2(Z_VM, p2_ez_run[:, 0:1], p2_ez_run[:, 1:2])
 
     p2_q2_mape_Queue = []
 
@@ -162,11 +137,11 @@ def main():
     print('Process-2 q2 Every Metrology MAPE After 15 Lot : {0:.2f}%'.format(np.mean(p2_q2_mape_Queue)))
     p2_q2_mape_Queue = []
 
-    # np.savetxt("output/p2_mape.csv", p2_q2_mape_Queue, delimiter=",", fmt="%.4f")
-    # np.savetxt("output/p2_ACT_Output.csv", VM_Output, delimiter=",", fmt="%.4f")
-    # np.savetxt("output/p1_y_prd.csv", p2_y_prd, delimiter=",", fmt="%.4f")
-    # np.savetxt("output/p1_y_act.csv", p2_y_act, delimiter=",", fmt="%.4f")
-    # np.savetxt("output/p2_ACT_Output.csv", ACT_Output, delimiter=",", fmt="%.4f")
+    np.savetxt("output/p2_mape.csv", p2_q2_mape_Queue, delimiter=",", fmt="%.4f")
+    np.savetxt("output/p2_ACT_Output.csv", Error_VMresult, delimiter=",", fmt="%.4f")
+    np.savetxt("output/p1_y_prd.csv", p2_y_prd, delimiter=",", fmt="%.4f")
+    np.savetxt("output/p1_y_act.csv", p2_y_act, delimiter=",", fmt="%.4f")
+    np.savetxt("output/p2_ACT_Output.csv", Error_ACTResult, delimiter=",", fmt="%.4f")
 
 
 if __name__ == "__main__":
